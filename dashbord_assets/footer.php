595d1c942c969fd590a8b63095dff010874246e8
Md. Sanaul hossen <73932888+sanaulhossen@users.noreply.github.com>
2020-12-29 11:35:37 +0600

    Add files via upload

---
+++ dashbord_assets/footer.php
@@ -0,0 +1,27 @@
+<!-- Footer START -->
+<div class="row">
+    <div class="col-md-8 m-auto">
+        <footer class="footer">
+            <div class="footer-content">
+                <p class="m-b-0">Copyright © 2019 Theme_Nate. All rights reserved.</p>
+                <span>
+                    <a href="" class="text-gray m-r-15">Term &amp; Conditions</a>
+                    <a href="" class="text-gray">Privacy &amp; Policy</a>
+                </span>
+            </div>
+        </footer>
+    </div>
+</div>
+        
+                <!-- Footer END -->
+    <!-- Core Vendors JS -->
+    <script src="/practice/dashbord_assets/js/vendors.min.js"></script>
+
+    <!-- page js -->
+
+    <!-- Core JS -->
+    <script src="/practice/dashbord_assets/js/app.min.js"></script>
+
+</body>
+
+</html>
---
+++ dashbord_assets/sidebar.php
@@ -0,0 +1,145 @@
+<!-- Side Nav START -->
+<div class="side-nav">
+    <div class="side-nav-inner">
+        <ul class="side-nav-menu scrollable">
+            <li class="nav-item dropdown">
+                <a href="/practice/dashbord.php">
+                    <span class="icon-holder">
+                        <i class="anticon anticon-dashboard"></i>
+                    </span>
+                    <span class="title">Dashbord</span>
+                </a>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="dropdown-toggle" href="javascript:void(0);">
+                    <span class="icon-holder">
+                        <i class="fab fa-chromecast"></i>
+                    </span>
+                    <span class="title">Hero Section</span>
+                    <span class="arrow">
+                        <i class="arrow-icon"></i>
+                    </span>
+                </a>
+                <ul class="dropdown-menu">
+                    <li>
+                        <a href="/practice/inc/hero_section/hero_view.php">Hero Description</a>
+                    </li>
+                    <li>
+                        <a href="/practice/inc/hero_social_link/social_link_view.php">Hero Social Link</a>
+                    </li>
+                </ul>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="dropdown-toggle" href="javascript:void(0);">
+                    <span class="icon-holder">
+                        <i class="fas fa-chalkboard-teacher"></i>
+                    </span>
+                    <span class="title">About Section</span>
+                    <span class="arrow">
+                        <i class="arrow-icon"></i>
+                    </span>
+                </a>
+                <ul class="dropdown-menu">
+                    <li>
+                        <a href="/practice/inc/about_section/about_view.php">About Me</a>
+                    </li>
+                    <li>
+                        <a href="/practice/inc/about_education/education_view.php">My Education</a>
+                    </li>
+                </ul>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="dropdown-toggle" href="javascript:void(0);">
+                    <span class="icon-holder">
+                        <i class="far fa-address-book"></i>
+                    </span>
+                    <span class="title">Contact Section</span>
+                    <span class="arrow">
+                        <i class="arrow-icon"></i>
+                    </span>
+                </a>
+                <ul class="dropdown-menu">
+                    <li>
+                        <a href="/practice/inc/contact_section/contact_view.php">Contact Information</a>
+                    </li>
+                    <li>
+                        <a href="/practice/inc/contact_form/contact_form_view.php">Contact Form</a>
+                    </li>
+                </ul>
+            </li>
+            <li class="nav-item dropdown">
+                <a href="/practice/inc/service_section/service_view.php">
+                    <span class="icon-holder">
+                        <i class="fab fa-servicestack"></i>
+                    </span>
+                    <span class="title">Service</span>
+                </a>
+            </li>
+            <li class="nav-item dropdown">
+                <a href="/practice/inc/testimonial_section/testimonial_view.php">
+                    <span class="icon-holder">
+                        <i class="far fa-comment-dots"></i>
+                    </span>
+                    <span class="title">Testimonial</span>
+                </a>
+            </li>
+            <li class="nav-item dropdown">
+                <a href="#">
+                    <span class="icon-holder">
+                        <i class="anticon anticon-dashboard"></i>
+                    </span>
+                    <span class="title">Reserved</span>
+                </a>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="dropdown-toggle" href="javascript:void(0);">
+                    <span class="icon-holder">
+                        <i class="anticon anticon-pie-chart"></i>
+                    </span>
+                    <span class="title">Dropdown</span>
+                    <span class="arrow">
+                        <i class="arrow-icon"></i>
+                    </span>
+                </a>
+                <ul class="dropdown-menu">
+                    <li>
+                        <a href="">Dropdown 1</a>
+                    </li>
+                    <li>
+                        <a href="">Dropdown 2</a>
+                    </li>
+                </ul>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="dropdown-toggle" href="javascript:void(0);">
+                    <span class="icon-holder">
+                        <i class="anticon anticon-file"></i>
+                    </span>
+                    <span class="title">Multi Level</span>
+                    <span class="arrow">
+                        <i class="arrow-icon"></i>
+                    </span>
+                </a>
+                <ul class="dropdown-menu">
+                    <li class="nav-item dropdown">
+                        <a href="javascript:void(0);">
+                            <span>Level 1</span>
+                            <span class="arrow">
+                                <i class="arrow-icon"></i>
+                            </span>
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li>
+                                <a href="">Level 2.1</a>
+                            </li>
+                            <li>
+                                <a href="">Level 2.2</a>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+    </div>
+</div>
+<!-- Side Nav END -->
