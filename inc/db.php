5fd1c953a5abb1a9e6a3405991a9cd97e4822aa0
Md. Sanaul hossen <73932888+sanaulhossen@users.noreply.github.com>
2020-12-29 11:26:01 +0600

    Add files via upload

---
+++ inc/db.php
@@ -0,0 +1,9 @@
+<?php
+
+	define("HOSTNAME", "localhost");
+    define("USERNAME", "root");
+    define("PASSWORD", "");
+    define("DBNAME", "kufa");
+
+    $db_connect = mysqli_connect(HOSTNAME,USERNAME,PASSWORD,DBNAME);
+?>
